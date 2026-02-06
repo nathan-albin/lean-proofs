f7960f0b920ea841007de778b855038b27db6714
Nathan Albin <34451964+nathan-albin@users.noreply.github.com>
2026-02-06 23:54:44 +0000

    Transitioning to finite ground set for now.

--- LeanProofs/Modulus.lean
+++ LeanProofs/Modulus.lean
@@ -2,13 +2,16 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.Algebra.BigOperators.Ring.Finset
 
-structure ObjectFamily (α : Type*) where
+-- working with finite gound sets for now
+variable {α : Type*} [Fintype α]
+
+structure ObjectFamily where
   -- objects live in a ground set
-  (E : Set α)
+  (E : Finset α)
   -- there is a usage function on objects
-  (N : Set α → α → ℝ)
+  (N : Finset α → α → ℝ)
   -- we can tell if a set is a member of the family
-  (IsObject : Set α → Prop)
+  (IsObject : Finset α → Prop)
   -- every object lives on E
   (subset_ground : ∀ γ, IsObject γ → γ ⊆ E)
   -- the usage function is non-negative
@@ -16,7 +19,7 @@ structure ObjectFamily (α : Type*) where
 
 namespace Modulus
 
-variable {α : Type*} [Fintype α] (Γ : ObjectFamily α)
+variable {α : Type*} [Fintype α] (Γ : ObjectFamily)
 
 def IsAdmissible (ρ : α → ℝ) : Prop :=
   ∀ γ, Γ.IsObject γ → (∑ e ∈ γ, ρ e ≥ 1)
