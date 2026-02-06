ebbd8d246c0da25623246af9c48836f604cb591a
Nathan Albin <34451964+nathan-albin@users.noreply.github.com>
2026-02-06 17:09:41 +0000

    Refactor BasePMF structure and related theorems for clarity and consistency

--- LeanProofs/Matroids.lean
+++ LeanProofs/Matroids.lean
@@ -1,74 +1,77 @@
 import Mathlib.Combinatorics.Matroid.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Combinatorics.Matroid.Rank.ENat
-import Mathlib.Data.Real.ENatENNReal
 import Mathlib.Algebra.BigOperators.Ring.Finset
 
 namespace Matroid
 
--- establish the scope, we're working with a finite matroid
-variable {α : Type*} [Fintype α] [DecidableEq α] (M : Matroid α)
+-- We're working with a finite ground set
+variable {α : Type*} [Fintype α]
 
-/-
-  The bases of a matroid are constructed by filtering the powerset
-  of the ground set.
--/
-noncomputable def bases (M : Matroid α) : Finset (Finset α) :=
-by
-  classical
-  exact (Finset.powerset (Finset.univ : Finset α)).filter
-    (fun B => M.IsBase (B : Set α))
+-- A probability mass function on the bases of a matroid.
+structure BasePMF (M : Matroid α) where
 
-#check bases M
-
-/-
-  A pmf on the bases of M is a function that assigns a non-negative real
-  number to each basis of M, such that the sum of these numbers over all
-  bases is 1. We can think of this as a probability distribution over the
-  bases of M.
--/
-structure PmfOnBases {α : Type*} [Fintype α] (M : Matroid α) where
+  -- assigns a real number to subsets of the ground set
   toFun : Finset α → ℝ
-  nonneg : ∀ B, 0 ≤ toFun B
-  sum_one : (∑ B ∈ (bases M), toFun B) = 1
 
-def usage (B : Finset α) (e : α) : ℕ :=
-  if e ∈ B then 1 else 0
+  -- is non-negative on all subsets
+  nonneg' : ∀ B, 0 ≤ toFun B
 
-noncomputable def expectedUsage (p : PmfOnBases M) (e : α) : ℝ :=
-  ∑ B ∈ (bases M), p.toFun B * (usage B e)
+  -- only bases have positive mass
+  support' : ∀ B, toFun B ≠ 0 → M.IsBase B
 
-theorem sum_usage_eq_rank (B : Finset α) (h : B ∈ (bases M)) :
-  ∑ e, usage B e = M.eRank.toNat := by
-  have hb : ∑ e, usage B e = B.card := by
-    unfold usage
-    rw [Finset.card_eq_sum_ones]
-    rw [← Finset.sum_filter]
-    simp
-  rw [hb]
-  #check Finset.mem_filter.mp h
-  rw [← Matroid.IsBase.encard_eq_eRank h]
+  -- the total mass sums to 1
+  sum_one' : ∑ B, toFun B = 1
 
+-- Allow PMF to act as a function.
+instance {M : Matroid α} : CoeFun (BasePMF M) (fun _ => Finset α → ℝ) where
+  coe p := p.toFun
 
+-- Helpful theorems.
+variable {M : Matroid α} (p : BasePMF M)
 
-theorem sum_expectedUsage (p : PmfOnBases M) :
-  ∑ e, expectedUsage M p e = M.eRank.toNat  := by
+@[simp] theorem BasePMF.nonneg (B : Finset α) : 0 ≤ p B :=
+  p.nonneg' B
+
+@[simp] theorem BasePMF.support (B : Finset α) :
+  p B ≠ 0 → M.IsBase B :=
+  p.support' B
+
+@[simp] theorem BasePMF.sum_one : ∑ B, p B = 1 :=
+  p.sum_one'
+
+section ExpectedUsage
+
+variable [DecidableEq α]
+
+def ExpectedUsage (e : α) : ℝ :=
+  ∑ B, p B * (if e ∈ B then 1 else 0)
+
+theorem sum_expected_usage :
+  ∑ e, ExpectedUsage p e = M.eRank.toNat := by
   calc
-    ∑ e, expectedUsage M p e
-        = ∑ e, ∑ B ∈ (bases M), p.toFun B * (usage B e) := by simp [expectedUsage]
-      _ = ∑ B ∈ (bases M), p.toFun B * (∑ e, usage B e) := by
+    ∑ e, ExpectedUsage p e
+        = ∑ e, ∑ B, p B * (if e ∈ B then 1 else 0) := by simp [ExpectedUsage]
+      _ = ∑ B, p B * (∑ e, if e ∈ B then 1 else 0) := by
         rw [Finset.sum_comm]
         simp_rw [Finset.mul_sum]
-      _ = ∑ B ∈ (bases M), p.toFun B * (M.eRank.toNat) := by
+      _ = ∑ B, p B * (M.eRank.toNat) := by
         apply Finset.sum_congr rfl
-        intro b h
-        rw [sum_usage_eq_rank]
-        grind
+        intro B h
+        rw [Finset.sum_ite]
+        simp only [Finset.filter_univ_mem, Finset.sum_const, nsmul_eq_mul, mul_one,
+          mul_eq_mul_left_iff]
+        by_cases hP : p.toFun B = 0
+        · simp [hP]
+        · have hB : M.IsBase B := p.support B hP
+          rw [← Matroid.IsBase.encard_eq_eRank hB]
+          left
+          simp
       _= M.eRank.toNat := by
         simp_rw [mul_comm]
         rw [← Finset.mul_sum]
         simp [p.sum_one]
 
-#check M.E
+end ExpectedUsage
 
 end Matroid
